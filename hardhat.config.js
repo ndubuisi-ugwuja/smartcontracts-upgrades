dfa786d01415185bf1c336225b510e5451b2ce11
Ndubuisi <ndujiovita1@gmail.com>
2025-12-13 17:55:02 +0100

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,43 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-ignition");
+require("@nomicfoundation/hardhat-verify");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+const {
+  SEPOLIA_RPC_URL,
+  PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+  COINMARKETCAP_API_KEY,
+} = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    etherscan: ETHERSCAN_API_KEY,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
+  mocha: {
+    timeout: 400000, // 400 seconds
+  },
 };
