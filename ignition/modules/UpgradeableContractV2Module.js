e2f6defb5fee72e23248501fbadaf9d0c2a1bb09
Ndubuisi <ndujiovita1@gmail.com>
2025-12-14 17:44:31 +0100

    fix deploy script

--- ignition/modules/UpgradeableContractV2Module.js
+++ ignition/modules/UpgradeableContractV2Module.js
@@ -1,6 +1,6 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-module.exports = buildModule("UpgradeableContractModuleV2", (m) => {
+module.exports = buildModule("UpgradeableContractV2Module", (m) => {
   const upgradeableContractV2 = m.contract("UpgradeableContractV2");
   return { upgradeableContractV2 };
 });
