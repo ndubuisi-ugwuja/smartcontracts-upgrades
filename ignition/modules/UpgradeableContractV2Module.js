09e10989fdda8664605c86cc387f1db1e858cf89
Ndubuisi <ndujiovita1@gmail.com>
2025-12-16 11:17:46 +0100

    remove unused ignition deploy script

--- ignition/modules/UpgradeableContractV2Module.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("UpgradeableContractV2Module", (m) => {
-    const upgradeableContractV2 = m.contract("UpgradeableContractV2");
-    return { upgradeableContractV2 };
-});
