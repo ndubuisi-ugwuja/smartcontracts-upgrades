528df01e65e34ced3fc766db04217466ccd2abcd
Ndubuisi <ndujiovita1@gmail.com>
2025-12-16 11:21:48 +0100

    add contract version 3

---
+++ contracts/UpgradeableContractV3.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract UpgradeableContractV2 {
+    uint256 internal value;
+
+    event ValueChanged(uint256 newValue);
+
+    function store(uint256 newValue) public {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
+
+    function version() public pure returns (uint8) {
+        return 3;
+    }
+
+    function increment() public {
+        value = value + 3;
+        emit ValueChanged(value);
+    }
+}
